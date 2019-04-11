4c09a7ec2c1bafe59d280cdba5edd86ec1211c0f
DoDatVu <datvu22@gmail.com>
2019-04-11 17:04:42 -0500

    hi

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger;
+CREATE TABLE burgers(
+id INT AUTO_INCREMENT,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOL,
+PRIMARY KEY(id)
+)
+
+CREATE TABLE burgers (
+
+INSERT INTO burgers (burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured);
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) values("Whopper", Bool);
+INSERT INTO burgers (burger_name, devoured) values("fish burger", Bool);
+INSERT INTO burgers (burger_name, devoured) values("deer burger", Bool);
